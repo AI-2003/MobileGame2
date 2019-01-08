878c35ca2bb154aa1fa2dfee2ebc5715206a32e9
IbarraranSoftware <46429550+IbarraranSoftware@users.noreply.github.com>
2019-01-07 20:42:12 -0600

    Color Script
    Day 01

---
+++ MobileGame2/Assets/My Assets/My Scripts/ColorTransitioner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTransitioner : MonoBehaviour
+{
+    public GameObject front;
+    public GameObject background;
+
+    public Gradient frontColors;
+    public Gradient backgroundColors;
+
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        front.GetComponent<Renderer>().material.color = frontColors.Evaluate(Mathf.PingPong(Time.time * speed,1));
+        background.GetComponent<Camera>().backgroundColor = backgroundColors.Evaluate(Mathf.PingPong(Time.time * speed, 1));
+    }
+}
