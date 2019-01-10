e908d15228dc7382a9f87de14160fc6aec4fd178
IbarraranSoftware <46429550+IbarraranSoftware@users.noreply.github.com>
2019-01-09 20:42:09 -0600

    Day 03
    Errors. Urgh.

---
+++ MobileGame2/Assets/My Assets/My Scripts/BallScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed;
+    private Vector2 rotation;
+    private Rigidbody ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Physics.Raycast(transform.position, new Vector2(Mathf.Abs(Mathf.PingPong(Time.time * speed, 1f) - 1f), Mathf.PingPong(Time.time * speed, 1f)));
+        Debug.DrawRay(transform.position, new Vector2(Mathf.Abs(Mathf.PingPong(Time.time * speed, 1f) - 1f), Mathf.PingPong(Time.time * speed, 1f)) * 10f, Color.blue);
+    }
+}
--- MobileGame2/Assets/My Assets/My Scripts/PlatformMovement.cs
+++ MobileGame2/Assets/My Assets/My Scripts/PlatformMovement.cs
@@ -5,24 +5,61 @@ using UnityEngine;
 public class PlatformMovement : MonoBehaviour
 {
     public Rigidbody platform;
+    public Rigidbody character;
     public float speed;
+    public bool ballLaunched = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Initial movement
         platform.velocity = new Vector2(0f, speed);
+        character.velocity = new Vector2(0f, speed);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //Moves it up and down
         if(platform.position.y > 3.5f)
         {
-            platform.velocity = new Vector2(0f, -speed);
+            platform.velocity = new Vector2(0f, 0f);
+            platform.position = new Vector2(-2.5f, 3.5f);
+            
+        }else if (platform.position.y < -3.5f)
+        {
+            platform.velocity = new Vector2(0f, 0f);
+            platform.position = new Vector2(-2.5f, -3.5f);
         }
 
-        if (platform.position.y < -3.5f)
+        if (!ballLaunched)
         {
-            platform.velocity = new Vector2(0f, speed);
+            if (character.position.y > 3.5f)
+            {
+                character.velocity = new Vector2(0f, 0f);
+                character.position = new Vector2(-2f, 3.5f);
+            }
+            else if (character.position.y < -3.5f)
+            {
+                character.velocity = new Vector2(0f, 0f);
+                character.position = new Vector2(-2f, -3.5f);
+            }
+
+            if (platform.position.y == character.position.y && platform.position.y == 3.5f)
+            {
+                platform.velocity = new Vector2(0f, -speed);
+                character.velocity = new Vector2(0f, -speed);
+            }
+            else if (platform.position.y == character.position.y && platform.position.y == -3.5f)
+            {
+                platform.velocity = new Vector2(0f, speed);
+                character.velocity = new Vector2(0f, speed);
+            }
+        }
+        else
+        {
+            platform.velocity = new Vector2(0f, 0f);
         }
     }
 }
